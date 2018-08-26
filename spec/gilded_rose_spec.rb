55c0790ef8941e855914ecb930648ad62f0d6662
Artyom Pertsovsky <artyom@pertsovsky.com>
2018-08-26 12:28:41 +0300

    Refactor project structure

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,10 +1,10 @@
 require_relative 'spec_helper'
 
-describe GildedRose do
+describe GildedRose::ItemCollection do
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      items = [GildedRose::Item.new("foo", 0, 0)]
+      GildedRose::ItemCollection.new(items).update_quality()
       expect(items[0].name).to eq "fixme"
     end
   end
